99c3fa2cc2cdc2e30ed1cf6577fa1bf3010cf99d
Randa-Khaled <320210601121@gmail.com>
2026-04-13 14:11:35 +0300

    code refactored

--- calculator.py
+++ calculator.py
@@ -1,32 +1,36 @@
-def add(x,y): return x+y
-def multiply(x,y): return x*y
-def sub(x,y): return x-y
-def divide(x,y): return x/y
+def add(x,y) :             
+    print(f"The summation of {x} and {y} = {x+y}")
+def multiply(x,y) : 
+    print(f"The multiplication of {x} and {y} = {x*y}")
+def sub(x,y) : 
+    print(f"The subtraction of {x} and {y} = {x-y}")
+def divide(x,y) : 
+    if y == 0 :
+        print("undefined! cannot divide by zero")
+    else : 
+        print(f"The division of {x} and {y} = {x/y}")
+
+print("Python Calculator")
 
 while True : 
-    print("Python Calculator")
     print("Please choose an operation to do : + , - , * , / or type 'exit' to exit the calculator")
     choice = input("Select an operation : ")
     if choice == 'exit' :
         print("closing the calculator..")
         break
     
-    if choice not in ['+', '-', '*', '/']:
-        print("Invalid operation selected! Please try again.")
-        continue
-    
     num1 = float(input("Please enter the first number : "))
     num2 = float(input("Please enter the second number : "))
     
     match choice : 
         case '+' :
-            print(f"The summation of {num1} and {num2} = {add(num1,num2)}")
+            add(num1,num2)
         case '-' :
-            print(f"The subtraction of {num1} and {num2} = {sub(num1,num2)}")
+            sub(num1,num2)
         case '*' : 
-            print(f"The multiplication of {num1} and {num2} = {multiply(num1,num2)}")
+            multiply(num1,num2)
         case '/' :
-            print(f"The division of {num1} and {num2} = {divide(num1,num2)}")
+            divide(num1,num2)
         case _ :
             print("Invalid operation selected!")
 
